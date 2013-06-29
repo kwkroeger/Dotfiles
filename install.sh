1ec8a20b06177d4d761516acc8343671380dc09f
Kurt Kroeger <kwkroeger@gmail.com>
2013-06-29 14:28:41 -0700

    Add installer and symlinker, add cat line numbering

---
+++ install.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+#########################
+#Dotfiles Install Script#
+#########################
+
+echo "Installing to $HOME"
+
+cp -r bashrc $HOME/.bashrc
+cp -r bash_profile $HOME/.bash_profile
+cp -r colordiff $HOME/.colordiff
+cp -r colordiffrc $HOME/.colordiffrc
+cp -r htop $HOME/.htop
+cp -r lesspipe $HOME/.lesspipe
+cp -r profile $HOME/.profile
+cp -r smem $HOME/.smem
+cp -r vim $HOME/.vim
+cp -r vimrc $HOME/.vimrc
+
+echo "Done"
---
+++ symlink.sh
@@ -0,0 +1,22 @@
+#/bin/sh
+
+#########################
+#Dotfiles Symlink Script#
+#########################
+
+echo "Linking to $HOME"
+
+export PWD=$(pwd)
+
+ln -s $PWD/bashrc $HOME/.bashrc
+ln -s $PWD/bash_profile $HOME/.bash_profile
+ln -s $PWD/colordiff $HOME/.colordiff
+ln -s $PWD/colordiffrc $HOME/.colordiffrc
+ln -s $PWD/htop $HOME/.htop
+ln -s $PWD/lesspipe $HOME/.lesspipe
+ln -s $PWD/profile $HOME/.profile
+ln -s $PWD/smem $HOME/.smem
+ln -s $PWD/vim $HOME/.vim
+ln -s $PWD/vimrc $HOME/.vimrc
+
+echo "Done"
